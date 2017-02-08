3ba4e48bc26d769a708e1763957a23a4302cc505
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-02-08 22:34:00 +0700

    New PNG, WebP and towebp.js

--- towebp.js
+++ towebp.js
@@ -4,9 +4,16 @@ var webp=require('webp-converter');
 //pass option(read  documentation for options) 
 
 //cwebp(input,output,option,result_callback) 
-files = ["x-hero-sm.png", "x-hero-lg.png"];
+files = ["x-hero-sm", "x-hero-lg"];
 for (var i = files.length - 1; i >= 0; i--) {
-	webp.cwebp("img/2x/" + files[i],"img/2x/" + files[i],"-q 80",function(status) {
+	webp.cwebp("img/" + files[i] + ".png","img/" + files[i] + ".webp","-q 80",function(status) {
+  	//if exicuted successfully status will be '100' 
+  	//if exicuted unsuccessfully status will be '101' 
+  	console.log(status);
+	});
+}
+for (var i = files.length - 1; i >= 0; i--) {
+	webp.cwebp("img/2x/" + files[i] + ".png","img/2x/" + files[i] + ".webp","-q 80",function(status) {
   	//if exicuted successfully status will be '100' 
   	//if exicuted unsuccessfully status will be '101' 
   	console.log(status);
