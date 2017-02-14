f8fefbcf977ef52589502678db920390819dad5b
zartre <zartre.y@gmail.com>
2017-02-14 21:45:26 +0700

    Add WebP for speaker images

--- towebp.js
+++ towebp.js
@@ -4,16 +4,16 @@ var webp=require('webp-converter');
 //pass option(read  documentation for options) 
 
 //cwebp(input,output,option,result_callback) 
-files = ["x-hero-sm", "x-hero-lg"];
+files = ["speaker-sm", "speaker-md", "speaker-lg"];
 for (var i = files.length - 1; i >= 0; i--) {
-	webp.cwebp("img/" + files[i] + ".png","img/" + files[i] + ".webp","-q 80",function(status) {
+	webp.cwebp("img/" + files[i] + ".jpg","img/" + files[i] + ".webp","-q 80",function(status) {
   	//if exicuted successfully status will be '100' 
   	//if exicuted unsuccessfully status will be '101' 
   	console.log(status);
 	});
 }
 for (var i = files.length - 1; i >= 0; i--) {
-	webp.cwebp("img/2x/" + files[i] + ".png","img/2x/" + files[i] + ".webp","-q 80",function(status) {
+	webp.cwebp("img/2x/" + files[i] + ".jpg","img/2x/" + files[i] + ".webp","-q 80",function(status) {
   	//if exicuted successfully status will be '100' 
   	//if exicuted unsuccessfully status will be '101' 
   	console.log(status);
