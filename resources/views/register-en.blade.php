e680ed8a165d8ce3d936e547732ecf224bdeb3fd
Foofy <foofybuster@gmail.com>
2017-03-11 22:40:16 +0700

    OK-FORM

---
+++ resources/views/register-en.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register for TEDxKMITL</title>
+    <link rel="stylesheet" href="{{asset('css/foundation.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/production.min.css')}}">
+</head>
+<body>
+<div class="row">
+    <div class="small-12 columns">
+        <h1 class="regis-header">TEDxKMITL Registration</h1>
+    </div>
+</div>
+<form action="">
+    <div class="row">
+        <div class="small-12 columns">
+            <label for="firstname">First name
+                <input type="text" name="firstname">
+            </label>
+        </div>
+    </div>
+</form>
+</body>
+</html>
