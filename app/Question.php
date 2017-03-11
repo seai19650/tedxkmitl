40f4fc3e827fc0614e6d1748c80ccaa4ed7896ea
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2017-03-11 23:30:27 +0700

    Normalize Database😍

--- app/Question.php
+++ app/Question.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    public function registration() {
-        return $this->belongsTo(Registration::class);
+    public function answer() {
+        return $this->hasOne(Answer::class);
     }
 }
