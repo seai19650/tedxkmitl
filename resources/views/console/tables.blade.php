75390ee32ea354ccde7cea847cd8ff053c27ddde
kavinvin <59070009@it.kmitl.ac.th>
2017-03-12 23:24:01 +0700

    Add console templates

---
+++ resources/views/console/tables.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.console')
+
+@section('header')
+<!-- Bootstrap -->
+<link href="production/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Font Awesome -->
+<link href="production/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<!-- NProgress -->
+<link href="production/vendors/nprogress/nprogress.css" rel="stylesheet">
+<!-- iCheck -->
+<link href="production/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+
+<!-- bootstrap-progressbar -->
+<link href="production/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+<!-- JQVMap -->
+<link href="production/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+<!-- bootstrap-daterangepicker -->
+<link href="production/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+
+<!-- Custom Theme Style -->
+<link href="production/build/css/custom.min.css" rel="stylesheet">
+@endsection('header')
+
+@section('content')
+
+@endsection('content')
