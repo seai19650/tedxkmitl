761b7ededcd16261a74b0e29bc46622186265021
zartre <zartre.y@gmail.com>
2017-03-12 10:54:16 +0700

    Add OG and TW Card to Regis page, add theme-color

--- resources/views/register-en.blade.php
+++ resources/views/register-en.blade.php
@@ -6,6 +6,16 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register for TEDxKMITL</title>
+    <meta name="theme-color" content="#e62b1a">
+    <meta property="og:image" content="{{asset('img/ogimage.png')}}">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="TEDxKMITL">
+    <meta name="twitter:description" content="TEDxKMITL is an independently organized TEDx event by KMITL students and personal. Our
+                main concept and theme of our event is “Living Out Loud” or LOL which is to bring out the speakers and
+                audiences full potential to show their inner self, what they truly have in them but doesn’t come out
+                very often.">
+    <meta name="twitter:image" content="{{asset('img/ogimage.png')}}">
+    <link rel="icon" href="{{asset('img/favicon.png')}}">
     <link rel="stylesheet" href="{{asset('css/foundation.min.css')}}">
     <link rel="stylesheet" href="{{asset('css/production.min.css')}}">
 </head>
