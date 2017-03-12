41626729101cfea4820ee28a6c5c69c78b8d8a80
zartre <zartre.y@gmail.com>
2017-03-12 11:57:13 +0700

    Fix 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -19,15 +19,28 @@
             margin: 100px auto 0;
             display: block;
         }
+        img#logo {
+            width: 230px;
+            display: block;
+            margin: 0 auto 50px;
+        }
         h1 {
             text-align: center;
             letter-spacing: 1px;
-            margin: 0;
+            margin: 0 0 50px;
         }
         hr {
             max-width: 500px;
             margin: 70px auto 60px;
             border: 2px solid #fff;
+        }
+        @media screen and (max-width: 374px) {
+            img#emoji {
+                margin-top: 50px;
+            }
+            hr {
+                margin: 40px auto 30px;
+            }
         }
     </style>
 </head>
@@ -35,5 +48,9 @@
 <img src="{{asset("img/404.svg")}}" alt="404" id="emoji">
 <hr>
 <h1>There's nothing here except you and me.</h1>
+<picture>
+    <source srcset="{{asset('img/logo-for-404.png')}} 1x, {{asset('img/2x/logo-for-404.png')}} 2x">
+    <img src="{{asset('img/logo-for-404.png')}}" alt="TEDxKMITL" id="logo">
+</picture>
 </body>
 </html>
