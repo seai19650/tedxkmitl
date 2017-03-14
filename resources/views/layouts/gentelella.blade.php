4950b7f8d75613e4e61e1c7dba4837b7e7b44334
kavinvin <59070009@it.kmitl.ac.th>
2017-03-14 22:57:38 +0700

    Link sidebar

--- resources/views/layouts/gentelella.blade.php
+++ resources/views/layouts/gentelella.blade.php
@@ -44,14 +44,14 @@
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Dashboard <span class="fa fa-chevron-down"></span></a>
+                  <li><a href="{{ route('console') }}"><i class="fa fa-home"></i> Dashboard <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="{{ route('console') }}">Dashboard</a></li>
                       <li><a href="index2.html">Dashboard2</a></li>
                       <li><a href="index3.html">Dashboard3</a></li>
                     </ul>
                   </li>
-                  <li><a><i class="fa fa-edit"></i> Registration <span class="fa fa-chevron-down"></span></a>
+                  <li><a href="{{ route('table') }}"><i class="fa fa-edit"></i> Registration <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
                       <li><a href="{{ route('table') }}">form</a></li>
                       <li><a href="form_advanced.html">Advanced Components</a></li>
