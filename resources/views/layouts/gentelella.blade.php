db57181836c3d2496150f7daaeb6b10b8f7d7b36
kavinvin <59070009@it.kmitl.ac.th>
2017-03-14 23:06:54 +0700

    Disable sidebar auto-slide

--- resources/views/layouts/gentelella.blade.php
+++ resources/views/layouts/gentelella.blade.php
@@ -44,25 +44,25 @@
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a href="{{ route('console') }}"><i class="fa fa-home"></i> Dashboard <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
+                  <li><a href="{{ route('console') }}"><i class="fa fa-home"></i> Dashboard </a>
+                    <!-- <ul class="nav child_menu">
                       <li><a href="{{ route('console') }}">Dashboard</a></li>
                       <li><a href="index2.html">Dashboard2</a></li>
                       <li><a href="index3.html">Dashboard3</a></li>
-                    </ul>
+                    </ul> -->
                   </li>
-                  <li><a href="{{ route('table') }}"><i class="fa fa-edit"></i> Registration <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
+                  <li><a href="{{ route('table') }}"><i class="fa fa-edit"></i> Registration </a>
+                    <!-- <ul class="nav child_menu">
                       <li><a href="{{ route('table') }}">form</a></li>
                       <li><a href="form_advanced.html">Advanced Components</a></li>
                       <li><a href="form_validation.html">Form Validation</a></li>
                       <li><a href="form_wizards.html">Form Wizard</a></li>
                       <li><a href="form_upload.html">Form Upload</a></li>
                       <li><a href="form_buttons.html">Form Buttons</a></li>
-                    </ul>
+                    </ul> -->
                   </li>
-                  <li><a><i class="fa fa-desktop"></i> Question <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
+                  <li><a><i class="fa fa-desktop"></i> Question </a>
+                    <!-- <ul class="nav child_menu">
                       <li><a href="general_elements.html">General Elements</a></li>
                       <li><a href="media_gallery.html">Media Gallery</a></li>
                       <li><a href="typography.html">Typography</a></li>
@@ -72,22 +72,22 @@
                       <li><a href="invoice.html">Invoice</a></li>
                       <li><a href="inbox.html">Inbox</a></li>
                       <li><a href="calendar.html">Calendar</a></li>
-                    </ul>
+                    </ul> -->
                   </li>
-                  <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
+                  <li><a><i class="fa fa-table"></i> Tables </a>
+                    <!-- <ul class="nav child_menu">
                       <li><a href="tables.html">Tables</a></li>
                       <li><a href="tables_dynamic.html">Table Dynamic</a></li>
-                    </ul>
+                    </ul> -->
                   </li>
-                  <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
+                  <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation </a>
+                    <!-- <ul class="nav child_menu">
                       <li><a href="chartjs.html">Chart JS</a></li>
                       <li><a href="chartjs2.html">Chart JS2</a></li>
                       <li><a href="morisjs.html">Moris JS</a></li>
                       <li><a href="echarts.html">ECharts</a></li>
                       <li><a href="other_charts.html">Other Charts</a></li>
-                    </ul>
+                    </ul> -->
                   </li>
                 </ul>
               </div>
