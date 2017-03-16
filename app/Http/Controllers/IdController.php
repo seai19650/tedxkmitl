e609d3f0ddc9950da59d009b5575f2afeb01cc92
Kasidis Chaowvasin <59070011@it.kmitl.ac.th>
2017-03-16 16:55:11 +0700

    Status Updated

--- app/Http/Controllers/IdController.php
+++ app/Http/Controllers/IdController.php
@@ -10,13 +10,7 @@ class IdController extends Controller
 {
     public function index($token) {
       $applicant = Registration::where('token', $token)->first();
-      $exist = Status::where('registration_id', $applicant->id)->first();
       if ($applicant == null) abort(404);
-      if ($exist == null) {
-          $accessed = 0;
-      } else {
-          $accessed = 1;
-      }
-      return view('id', compact('applicant', 'accessed'));
+      return view('id', compact('applicant' ));
     }
 }
