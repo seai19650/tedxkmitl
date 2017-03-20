e6e2c8296f3c61b7960160afc01b343022a22949
kavinvin <59070009@it.kmitl.ac.th>
2017-03-20 15:44:09 +0700

    Select column before return ajax

---
+++ config/ajax.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    // internal applicantion column selected to return through AJAX
+    'selection' => [
+        'id',
+        'firstname',
+        'lastname',
+        'age',
+        'mobile',
+        'email',
+        'gender',
+        'ticket_type',
+        'is_approved',
+        'is_paid',
+        'created_at'
+    ]
+];
