b696a820950e850e54de2b091defd40f407c2dc1
kavinvin <59070009@it.kmitl.ac.th>
2017-03-20 17:11:04 +0700

    Fix logout button

--- resources/views/layouts/gentelella.blade.php
+++ resources/views/layouts/gentelella.blade.php
@@ -109,18 +109,14 @@
 
             <!-- /menu footer buttons -->
             <div class="sidebar-footer hidden-small">
-              <a data-toggle="tooltip" data-placement="top" title="Settings">
-                <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+              <a data-toggle="tooltip" data-placement="top" title="Logout" href="{{ secure_url('/logout') }}"
+                  onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();"><span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
-              <a data-toggle="tooltip" data-placement="top" title="FullScreen">
-                <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Lock">
-                <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
-              </a>
-              <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-              </a>
+
+              <form id="logout-form" action="{{ secure_url('/logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+              </form>
             </div>
             <!-- /menu footer buttons -->
           </div>
