0691ce6b4efc93c64a37789817453f6dbb89dd82
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-04-02 17:41:09 +0700

    Finish e-ticket mail

--- resources/views/mail/mail_eticket.blade.php
+++ resources/views/mail/mail_eticket.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1, width=device-width">
-    <title>E-Ticket</title>
+    <title>Here is your TEDxKMITL e-ticket</title>
     <style>
         html {
             font-family: "Helvetica Neue", Arial, sans-serif;
@@ -11,53 +11,36 @@
         body {margin: 0;}
         .wrap {
             margin: 0 auto;
-            width: 100%;
-            max-width: 600px;
-            padding: 1.5em 1em 1em;
+            width: 300px;
+            height: 534px;
+            background: url("{{asset('img/e-ticket.png')}}");
+            background-size: contain;
             -webkit-box-sizing: border-box;
             -moz-box-sizing: border-box;
             box-sizing: border-box;
+            padding: 10px;
         }
-        img#logo {width: 240px;}
-        hr {
-            width: 100%;
-            max-width: 240px;
-            margin: 1em 0 1.4em;
-            border: 3px solid #e62b1a;
-        }
-        h1 {font-size: 2em;}
-        p {font-size: 16px;}
-        p.start-end {
-            font-size: 18px;
-            font-family: serif;
-            font-style: italic;
-            color: #a1a1a1;
+        h1,h3,h4,h5 {
+            text-align: right;
+            text-transform: uppercase;
+            position: relative;
         }
     </style>
 </head>
 <body>
 <div class="wrap">
-    <img src="{{ $message->embed(public_path() . '/img/2x/nav-on-w.png') }}" alt="TEDxKMITL" id="logo">
-    <h1>This is your E-Ticket!</h1>
-    <hr>
-    <p class="start-end">Dear {{ $applicant->firstname }},</p>
-    <p>Congratulations! You are eligible to be an audience in the <strong>Studio</strong> room. We are so thrilled to
-        invite you on board.</p>
-    <p>The next step is to pay for your ticket.</p>
-    <div>
-        <p><b>Account No.</b> 693-0-26770-8</p>
-        <p><b>Krungthai Bank</b> สาขาเทคโนโลยีพระจอมเกล้าฯลาดกระบัง</p>
-        <p><b>Account name:</b> นายกันยภัทร ทองมา และ นายดุสทิตต์ ลาภสนาเปรม และ น.ส.วิสุทธิ์รัตน์ รุ่งนพคุณศรี</p>
+    <div style="position:relative;top:69px;text-align:center;">
+        {!! QrCode::size(100)->margin(0)->generate('http://tedxkmitl.com/') !!}
     </div>
-    <p>Please note that your seat will be revoked if the payment is not fulfilled within 3 days from now (before March 28 at 4.00 PM).</p>
-    <p>Please provide your payment slip by sending an e-mail to <a href="mailto:tedxkmtil@gmail.com">tedxkmitl@gmail.com</a> and <strong>PLEASE WRITE YOUR FULL NAME IN ENGLISH ON THE SLIP</strong></p>
-    <p>We'd love to see you there!</p>
-    <p class="start-end">Yours sincerely,<br>TEDxKMITL Staff</p>
-    <hr>
-    <p><strong>Email:</strong> <a href="mailto:tedxkmitl@gmail.com">tedxkmitl@gmail.com</a><br>
-        <strong>Facebook Page:</strong> <a href="www.facebook.com/tedxkmitl">TEDxKMITL</a><br>
-        <strong>Tel. BON (คุณบอน):</strong> 06-5159-5696<br>
-        <strong>Tel. TO (คุณโต้):</strong> 09-2579-6660</p>
+    <h4 style="top:120px;">April 22, 2017</h4>
+    <h3 style="top:100px;">Livin out Loud</h3>
+    <h5 style="top:100px;color:#939393;">Nickname</h5>
+    <h1 style="top:80px;">Zartre</h1>
+    <h5 style="top:77px;color:#939393;">Full name</h5>
+    <h4 style="top:57px;">Nathan</h4>
+    <h4 style="top:37px;">Yiangsupapaanontr</h4>
+    <h5 style="top:37px;color:#939393;">Type</h5>
+    <h3 style="top:17px;">General</h3>
 </div>
 </body>
 </html>
