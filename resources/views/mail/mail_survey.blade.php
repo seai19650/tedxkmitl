821e043ef3f7ac49f20c55c392bedd3c7ef61987
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2017-04-22 18:06:48 +0700

    Add Survey email

---
+++ resources/views/mail/mail_survey.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html style="font-family: 'Helvetica Neue', Arial, sans-serif;">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="initial-scale=1, width=device-width">
+    <title>Help us improve TEDxKMITL</title>
+</head>
+<body style="margin: 0;">
+<div style="margin:0 auto;width:100%;max-width:600px;padding:1.5em 1em 1em 1em;">
+    <img src="{{ $message->embed(public_path() . '/img/2x/nav-on-w.png') }}" alt="TEDxKMITL" style="width: 240px;">
+    <h1 style="font-size: 2em;">Help us improve TEDxKMITL</h1>
+    <hr style="width:100%;max-width:240px;margin:1em 0 1.4em;border:3px solid #e62b1a;">
+    <p style="font-size: 18px;font-family: serif;font-style: italic;color: #a1a1a1;">Dear {{ $applicant->firstname }},</p>
+    <p style="font-size: 16px;">Thank you for joining TEDxKMITL 2017. Please take some minutes to help us improve TEDxKMITL even further. Follow the link below to take your part.</p>
+    <p style="font-size: 16px;">ขอขอบพระคุณเป็นอย่างสูงที่ให้ความสนใจในงาน TEDxKMITL 2017 ครั้งนี้ ความคิดเห็นของท่านเป็นสิ่งสำคัญที่จะทำให้เราปรับปรุงงาน TEDxKMITL ได้ดียิ่งขึ้น ท่านสามารถแสดงความคิดเห็นกับงานครั้งนี้ได้ที่ลิงก์ด้านล่าง</p>
+    <div style="text-align: center;">
+        <a href="#"
+           style="text-decoration: none;background: #e62b1a;padding: 1em;margin: 0.5em auto 0.5em auto;display: inline-block;color: #fff;font-weight: bold;">Be a part</a>
+    </div>
+    <p style="font-size: 18px;font-family: serif;font-style: italic;color: #a1a1a1;">Thank you for your time,<br>TEDxKMITL Staff</p>
+    <p style="font-size: 18px;font-family: serif;font-style: italic;color: #a1a1a1;">ขอบคุณที่สละเวลา<br>ทีมงาน TEDxKMITL</p>
+    <hr style="width:100%;max-width:240px;margin:1em 0 1.4em;border:3px solid #e62b1a;">
+    <p style="font-size: 16px;"><strong>Email:</strong> <a href="mailto:tedxkmitl@gmail.com">tedxkmitl@gmail.com</a><br>
+        <strong>Facebook Page:</strong> <a href="https://fb.me/TEDxKMITL">TEDxKMITL</a><br>
+        <strong>Tel. BON (คุณบอน):</strong> 06-5159-5696<br>
+        <strong>Tel. TO (คุณโต้):</strong> 09-2579-6660</p>
+</div>
+</body>
+</html>
